2bf01157a227751615df8e9edb73c8374057c4e2
Rivalll-D <103737970+Rivalll-D@users.noreply.github.com>
2022-07-21 10:15:57 +0700

    Update migration.js

--- src/conf/migration.js
+++ src/conf/migration.js
@@ -1,5 +1,5 @@
 const migration = require('mysql-migrations');
-const Database = require('./Database');
+const Database = require('./database');
 
 const database = new Database();
 migration.init(database.connection, __dirname + '/migrations');
